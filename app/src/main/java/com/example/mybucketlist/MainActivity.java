eb11c4087439dce40f3e21dad8c02422642d7363
Catalin Gavrila <cata_bb92@yahoo.com>
2023-05-11 09:00:25 +0300

    added layouts and activities for things to do and places to go

--- app/src/main/java/com/example/mybucketlist/MainActivity.java
+++ app/src/main/java/com/example/mybucketlist/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mybucketlist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView cardViewThingsToDo = findViewById(R.id.card_view_things_to_do);
+        CardView cardViewPlacesToGo = findViewById(R.id.card_view_places_to_go);
+
+        cardViewThingsToDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent thingsToDoIntent = new Intent(MainActivity.this, ThingsToDoActivity.class);
+                startActivity(thingsToDoIntent);
+            }
+        });
+
+        cardViewPlacesToGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent placesToGoIntent = new Intent(MainActivity.this, PlacesToGoActivity.class);
+                startActivity(placesToGoIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mybucketlist/PlacesToGoActivity.java
@@ -0,0 +1,14 @@
+package com.example.mybucketlist;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlacesToGoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_to_go);
+    }
+}
