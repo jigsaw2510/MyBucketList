345df9ecc92b2583cc3d63b4bdd7ab50c5694f12
Catalin Gavrila <cata_bb92@yahoo.com>
2023-05-11 09:41:18 +0300

    implemented recycler view adapter and view holder functionality

--- app/src/main/java/com/example/mybucketlist/PlacesAdapter.java
+++ app/src/main/java/com/example/mybucketlist/PlacesAdapter.java
@@ -1,5 +1,33 @@
 package com.example.mybucketlist;
 
-public class PlacesAdapter {
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PlacesAdapter extends RecyclerView.Adapter<PlacesAdapter.PlaceViewHolder> {
+
+
+    @NonNull
+    @Override
+    public PlaceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlaceViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class PlaceViewHolder extends RecyclerView.ViewHolder {
+        public PlaceViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/example/mybucketlist/ThingsAdapter.java
+++ app/src/main/java/com/example/mybucketlist/ThingsAdapter.java
@@ -1,4 +1,34 @@
 package com.example.mybucketlist;
 
-public class ThingsAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ThingsAdapter extends RecyclerView.Adapter<ThingsAdapter.ThingViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ThingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ThingViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ThingViewHolder extends RecyclerView.ViewHolder {
+        public ThingViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
