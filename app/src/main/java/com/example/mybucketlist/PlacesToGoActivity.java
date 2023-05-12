425311d5a041ff75c0e249fa9e78c3f5aebe134f
Catalin Gavrila <cata_bb92@yahoo.com>
2023-05-12 13:13:12 +0300

    a bit more refactoring

--- app/src/main/java/com/example/mybucketlist/PlacesToGoActivity.java
+++ app/src/main/java/com/example/mybucketlist/PlacesToGoActivity.java
@@ -11,18 +11,34 @@ public class PlacesToGoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_to_go);
+        setupList();
+    }
 
+    private void setupList() {
         RecyclerView list =findViewById(R.id.recycler_view_places);
-
         Place[] places ={
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
+                new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
                 new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
                 new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
                 new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go),
                 new Place("Japonia", "Un loc in care vreau sa ajung de cand am aflat despre cultura japonezilor", R.drawable.icon_places_to_go)
         };
-
         PlacesAdapter adapter = new PlacesAdapter(places);
         list.setAdapter(adapter);
-
     }
 }
--- app/src/main/java/com/example/mybucketlist/ThingsToDoActivity.java
+++ app/src/main/java/com/example/mybucketlist/ThingsToDoActivity.java
@@ -11,17 +11,29 @@ public class ThingsToDoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_things_to_do);
+        setupList();
+    }
 
+    private void setupList() {
         RecyclerView list = findViewById(R.id.recycler_view_things);
-
         Thing[] things = {
+                new Thing("Own a foosball business", "One of my dreams since I found out about foosball", R.drawable.icon_things_to_do),
+                new Thing("Get the Associate Android Developer certificate", "Would be good for starting a career in Android Development", R.drawable.icon_things_to_do),
+                new Thing("Get the Oracle Certified Associate certificate", "Would be good for starting a career in IT", R.drawable.icon_things_to_do),
+                new Thing("Get the Android Certified Application Developer certificate", "The next step in pursuing an Android Developer career", R.drawable.icon_things_to_do),
+                new Thing("Own a foosball business", "One of my dreams since I found out about foosball", R.drawable.icon_things_to_do),
+                new Thing("Get the Associate Android Developer certificate", "Would be good for starting a career in Android Development", R.drawable.icon_things_to_do),
+                new Thing("Get the Oracle Certified Associate certificate", "Would be good for starting a career in IT", R.drawable.icon_things_to_do),
+                new Thing("Get the Android Certified Application Developer certificate", "The next step in pursuing an Android Developer career", R.drawable.icon_things_to_do),
+                new Thing("Own a foosball business", "One of my dreams since I found out about foosball", R.drawable.icon_things_to_do),
+                new Thing("Get the Associate Android Developer certificate", "Would be good for starting a career in Android Development", R.drawable.icon_things_to_do),
+                new Thing("Get the Oracle Certified Associate certificate", "Would be good for starting a career in IT", R.drawable.icon_things_to_do),
+                new Thing("Get the Android Certified Application Developer certificate", "The next step in pursuing an Android Developer career", R.drawable.icon_things_to_do),
                 new Thing("Own a foosball business", "One of my dreams since I found out about foosball", R.drawable.icon_things_to_do),
                 new Thing("Get the Associate Android Developer certificate", "Would be good for starting a career in Android Development", R.drawable.icon_things_to_do),
                 new Thing("Get the Oracle Certified Associate certificate", "Would be good for starting a career in IT", R.drawable.icon_things_to_do),
                 new Thing("Get the Android Certified Application Developer certificate", "The next step in pursuing an Android Developer career", R.drawable.icon_things_to_do)
         };
-
-
         ThingsAdapter adapter = new ThingsAdapter(things);
         list.setAdapter(adapter);
     }
